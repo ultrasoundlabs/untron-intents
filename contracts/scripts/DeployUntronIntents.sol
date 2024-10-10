47a43ec0bf283859166336483eaeec201e282e54
ziemen4 <173351113+ziemen4@users.noreply.github.com>
2024-10-11 00:47:57 +0200

    Add test script and support EIP-712 for nested structs

---
+++ contracts/scripts/DeployUntronIntents.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.20;
+
+import "forge-std/console.sol";
+import "forge-std/Script.sol";
+import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+import "../src/chains/MockUntronIntents.sol";
+import "../src/UntronIntentsProxy.sol";
+
+contract DeployUntronIntents is Script {
+    UntronIntentsProxy proxy;
+    MockUntronIntents wrappedProxyV1;
+
+    function run() public {
+        UntronIntents implementationV1 = new MockUntronIntents();
+        
+        // deploy proxy contract and point it to implementation
+        proxy = new UntronIntentsProxy(address(implementationV1), address(this), "");
+        
+        // wrap in ABI to support easier calls
+        wrappedProxyV1 = MockUntronIntents(address(proxy));
+        wrappedProxyV1.initialize(address(0x0));
+
+        console.log("UntronIntentsProxy deployed at address: ", address(proxy));
+    }
+}
