fd881dae2545159b74a59c8e681709f03d9d8524
jordan <41rjordan@proton.me>
2026-01-09 16:18:24 +0500

    feat: init new project

---
+++ README.md
@@ -0,0 +1,3 @@
+# Untron Intents
+
+TODO
---
+++ wagmi.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from "@wagmi/cli";
+import { foundry, foundryDefaultExcludes } from "@wagmi/cli/plugins";
+
+// Determine artifacts directory based on FOUNDRY_PROFILE env variable
+// const profile = process.env.FOUNDRY_PROFILE ?? "dev";
+const artifactsPath = `out`;
+
+export default defineConfig({
+  out: "packages/contracts/abi/generated.ts",
+  plugins: [
+    foundry({
+      project: "packages/contracts", // <— your Foundry project root
+      artifacts: artifactsPath,
+      exclude: [
+        // Start from wagmi defaults, but keep IERC20's ABI.
+        ...foundryDefaultExcludes.filter((x) => x !== "IERC20.sol/**"),
+
+        // Extra exclusions for this repo.
+        "**/*.dbg.json",
+        "auth/**", // e.g. solady's auth/Ownable
+        "interfaces/**", // e.g. placeholder interfaces with empty ABI
+        "**/IMulticall3.sol/**",
+      ],
+      // forge: { build: true } // default; Wagmi can run forge build for you
+    }),
+  ],
+});
